4b8be2aba25b9bdd68bbafa3990e054a74f3cc23
malloc <aleco@aroltd.com>
2018-08-08 15:14:05 -0500

    wot

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
+#include "SDL.h"
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+    SDL_Surface* screen;
 
+    if(SDL_Init(SDL_INIT_VIDEO) == -1)
+        return -1;
+    atexit(SDL_Quit);
+
+    screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);
+    if(screen == NULL)
+        return -1;
+
+    // if you are reading this your usc
+
+    return 0;
 }
